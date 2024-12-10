fff520456d4569084dd9722d0160fedbb8fa37dc
Pablo Alan <pabloxt14@gmail.com>
2024-12-10 19:54:59 -0300

    feat: create welcome component

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,9 +1,10 @@
+import { Welcome } from '@/components/welcome'
 import { Text, View } from 'react-native'
 
 export default function Index() {
   return (
-    <View>
-      <Text>Hello world</Text>
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
     </View>
   )
 }
