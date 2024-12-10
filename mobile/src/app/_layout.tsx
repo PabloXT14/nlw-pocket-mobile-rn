f0a7ab4170765ff73725ff98a8729f2aa86ca877
Pablo Alan <pabloxt14@gmail.com>
2024-12-10 19:12:15 -0300

    feat: config app fonts

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,8 +1,26 @@
 import { Stack } from 'expo-router'
+import {
+  useFonts,
+  Rubik_400Regular,
+  Rubik_500Medium,
+  Rubik_600SemiBold,
+  Rubik_700Bold,
+} from '@expo-google-fonts/rubik'
 
 import { colors } from '@/styles/theme'
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_600SemiBold,
+    Rubik_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
     <Stack
       screenOptions={{
