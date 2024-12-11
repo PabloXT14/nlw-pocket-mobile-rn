311def61067b2d16da4018fa38d13035c0fcdc06
Pablo Alan <pabloxt14@gmail.com>
2024-12-11 11:40:27 -0300

    feat: config api service to consume on mobile

--- mobile/src/app/home.tsx
+++ mobile/src/app/home.tsx
@@ -1,6 +1,26 @@
-import { Text, View } from 'react-native'
+import { useEffect, useState } from 'react'
+import { Alert, Text, View } from 'react-native'
+
+import { getCategories } from '@/http/get-categories'
 
 export default function Home() {
+  const [categories, setCategories] = useState([])
+
+  async function fetchCategories() {
+    try {
+      const categories = await getCategories()
+      console.log('CATEGORIAS: ', categories)
+      setCategories(categories)
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Categorias', 'Não foi possível carregar as categorias.')
+    }
+  }
+
+  useEffect(() => {
+    fetchCategories()
+  }, [])
+
   return (
     <View style={{ flex: 1 }}>
       <Text>Home</Text>
