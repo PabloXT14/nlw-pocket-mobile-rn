bd9f2e44870a3fbda9387c72b274b134172dfdd0
Pablo Alan <pabloxt14@gmail.com>
2024-12-11 20:34:05 -0300

    fear: config expo-map-view

--- mobile/src/app/home.tsx
+++ mobile/src/app/home.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { Alert, View } from 'react-native'
+import MapView from 'react-native-maps'
 
 import type { CategoryDTO } from '@/@types/category'
 import type { PlaceDTO } from '@/@types/place'
@@ -8,6 +9,11 @@ import { getMarketsByCategoryId } from '@/http/get-markets-by-category-id'
 
 import { Categories } from '@/components/categories'
 import { Places } from '@/components/places'
+
+const currentLocation = {
+  latitude: -23.561187293883442,
+  longitude: -46.656451388116494,
+}
 
 export default function Home() {
   const [categories, setCategories] = useState<CategoryDTO[]>([])
@@ -18,6 +24,7 @@ export default function Home() {
   async function fetchCategories() {
     try {
       const categories = await getCategories()
+
       setCategories(categories)
       setSelectedCategoryId(categories[0].id)
     } catch (error) {
@@ -57,6 +64,16 @@ export default function Home() {
         data={categories}
         selectedCategoryId={selectedCategoryId}
         onCategorySelect={setSelectedCategoryId}
+      />
+
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+          latitudeDelta: 0.01, // aproximação/zoom
+          longitudeDelta: 0.01,
+        }}
       />
 
       <Places data={markets} />
