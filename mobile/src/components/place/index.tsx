edff6ff5f9a1a7f7dd02b631b423c5c2c3bffcd0
Pablo Alan <pabloxt14@gmail.com>
2024-12-11 19:54:47 -0300

    feat: create place component

---
+++ mobile/src/components/place/index.tsx
@@ -0,0 +1,36 @@
+import {
+  Image,
+  Text,
+  TouchableOpacity,
+  type TouchableOpacityProps,
+  View,
+} from 'react-native'
+import { IconTicket } from '@tabler/icons-react-native'
+
+import type { PlaceDTO } from '@/@types/place'
+
+import { colors } from '@/styles/theme'
+import { s } from './styles'
+
+type PlaceProps = TouchableOpacityProps & {
+  data: PlaceDTO
+}
+
+export function Place({ data, ...props }: PlaceProps) {
+  return (
+    <TouchableOpacity style={s.container} {...props} activeOpacity={0.8}>
+      <Image style={s.image} source={{ uri: data.cover }} />
+
+      <View style={s.content}>
+        <Text style={s.name}>{data.name}</Text>
+
+        <Text style={s.description}>{data.description}</Text>
+
+        <View style={s.footer}>
+          <IconTicket size={16} color={colors.red.base} />
+          <Text style={s.tickets}>{data.coupons} cupons disponíveis</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  )
+}
